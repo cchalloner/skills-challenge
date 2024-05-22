5d2a20aada2b9c16987f4eca89de23424b22e767
Carolyn Challoner <16659436+cchalloner@users.noreply.github.com>
2024-05-22 21:49:49 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,99 +1,38 @@
-// create web server
-const express = require("express");
+// create web server with express
+const express = require('express');
 const app = express();
 const port = 3000;
 
-// import mysql
-const mysql = require("mysql");
+// use express to parse JSON data
+app.use(express.json());
 
-// create connection
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "comments",
+// create an array of comments
+const comments = [
+  { id: 1, author: 'John', text: 'First comment!' },
+  { id: 2, author: 'Jane', text: 'Second comment!' },
+  { id: 3, author: 'Joe', text: 'Third comment!' },
+  { id: 4, author: 'Jill', text: 'Fourth comment!' },
+];
+
+// create a GET route to return all comments
+app.get('/comments', (req, res) => {
+    res.json(comments);
 });
 
-// connect to database
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Connected!");
-  }
+// create a GET route to return a specific comment
+app.get('/comments/:id', (req, res) => {
+    const comment = comments.find(c => c.id === parseInt(req.params.id));
+    if (!comment) return res.status(404).send('The comment with the given ID was not found.');
+    res.json(comment);
 });
 
-// get all comments
-app.get("/comments", (req, res) => {
-  connection.query("SELECT * FROM comments", (err, results) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.json(results);
-    }
-  });
+// create a POST route to add a new comment
+app.post('/comments', (req, res) => {
+    const comment = {
+        id: comments.length + 1,
+        // add other properties here
+    };
+    // add comment to the comments array
+    comments.push(comment);
+    res.json(comment);
 });
-
-// get comment by id
-app.get("/comments/:id", (req, res) => {
-  connection.query(
-    "SELECT * FROM comments WHERE id = ?",
-    [req.params.id],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json(results);
-      }
-    }
-  );
-});
-
-// create comment
-app.post("/comments", (req, res) => {
-  const { body } = req;
-  connection.query(
-    "INSERT INTO comments (comment) VALUES (?)",
-    [body.comment],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json({ id: results.insertId });
-      }
-    }
-  );
-});
-
-// update comment by id
-app.put("/comments/:id", (req, res) => {
-  const { body } = req;
-  connection.query(
-    "UPDATE comments SET comment = ? WHERE id = ?",
-    [body.comment, req.params.id],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json({ id: req.params.id });
-      }
-    }
-  );
-});
-
-// delete comment by id
-app.delete("/comments/:id", (req, res) => {
-  connection.query(
-    "DELETE FROM comments WHERE id = ?",
-    [req.params.id],
-    (err, results) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.json({ id: req.params.id });
-      }
-    }
-  );
-});
-
-// listen to port
